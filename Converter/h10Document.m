d3b9e5c6eb56488ecf6cbbd899767125c6271159
Martin Snajdr <martin@hundert10.com>
2013-02-26 20:44:36 +0100

    Initial Commit

---
+++ Converter/h10Document.m
@@ -0,0 +1,40 @@
+//
+//  h10Document.m
+//  Converter
+//
+//  Created by Martin Snajdr on 26.02.13.
+//  Copyright (c) 2013 hundert10 Medien GmbH. All rights reserved.
+//
+
+#import "h10Document.h"
+
+@implementation h10Document
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"h10Document";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
++ (BOOL)autosavesInPlace
+{
+    return YES;
+}
+
+@end
---
+++ Converter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Converter
+//
+//  Created by Martin Snajdr on 26.02.13.
+//  Copyright (c) 2013 hundert10 Medien GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
